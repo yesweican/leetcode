52d4fb6d5ad43c3ad2ac68b2b4013dfa518b5bf0
yesweican <40693861+yesweican@users.noreply.github.com>
2019-04-19 19:59:23 -0600

    q0145

---
+++ csharpe/q0145/Traversal2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConstructBinaryTreeApp
+{
+    public class Traversal2
+    {
+        public void PostOrder(TreeNode root)
+        {
+            List<int> results = new List<int>();
+
+            if (root == null)
+            {
+                //return results;
+                return;
+            }
+
+            Stack s1 = new Stack();
+            Stack s2 = new Stack();
+
+            s1.Push(root);
+
+            while (s1.Count > 0)
+            {
+                TreeNode temp = (TreeNode)s1.Pop();
+
+                s2.Push(temp);
+
+                if (temp.left != null)
+                    s1.Push(temp.left);
+                if (temp.right != null)
+                    s1.Push(temp.right);
+            }
+
+            while(s2.Count>0)
+            {
+                TreeNode temp = (TreeNode)s2.Pop();
+                results.Add(temp.val);
+                Console.Write(temp.val);
+            }
+        }
+    }
+}
