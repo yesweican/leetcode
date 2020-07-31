141d7fd0a5a7df8863055da96ae4e91931a4cf81
Wei Cheng <40693861+yesweican@users.noreply.github.com>
2020-07-30 22:00:25 -0600

    Replaced a solution to a hard question

---
+++ csharpe/q0140/q140.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeChallenge04
+{
+    public class q140
+    {
+        HashSet<string> dict;
+        Dictionary<string, List<string>> wordBreaks;
+
+        public IList<string> WordBreak(string s, IList<string> wordDict)
+        {
+            dict = new HashSet<string>();
+            wordBreaks = new Dictionary<string, List<string>>();
+
+            foreach(var v in wordDict)
+            {
+                dict.Add(v);
+            }
+
+            RecursiveWordBreak(s);
+
+            return ((IList<string>)wordBreaks[s]);
+        }
+
+        private void RecursiveWordBreak(string s)
+        {
+            List<string> result = new List<string>();
+
+            //if (wordBreaks.ContainsKey(s))
+             //   return;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                string temp = s.Substring(i, s.Length - i);
+
+                if(dict.Contains(temp))
+                {
+                    if (i == 0)
+                        result.Add(temp);
+                    else
+                    {
+                        string temp2 = s.Substring(0, i);
+
+                        if(!wordBreaks.ContainsKey(temp2))
+                        {
+                            RecursiveWordBreak(temp2);
+                        }
+
+                        foreach(var v in wordBreaks[temp2])
+                        {
+                            result.Add(v + " " + temp);
+                        }
+
+                    }
+                }
+            }
+
+            wordBreaks.Add(s, result);
+        }
+
+    }
+}
