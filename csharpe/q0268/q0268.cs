962f8fa6650ed3288604cd9c3ee247b2fa824a64
Wei Cheng <40693861+yesweican@users.noreply.github.com>
2020-08-05 01:05:17 -0600

    Adding a couple more

---
+++ csharpe/q0268/q0268.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeChallenge05
+{
+    public class q268
+    {
+        //not so opimized
+        public int MissingNumber(int[] nums)
+        {
+            int[] newArray = new int[nums.Length + 1];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                newArray[nums[i]] = nums[i] + 1;
+            }
+
+            for (int j = 0; j < (nums.Length + 1); j++)
+            {
+                if (newArray[j] == 0)
+                    return j;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ csharpe/q0342/q0342.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeChallenge05
+{
+    public class q342
+    {
+        public bool IsPowerOfFour(int num)
+        {
+            if (num == 0)
+                return false;
+
+            while(num>0)
+            {
+                int temp = num & 3;
+                if (temp != 0)
+                {
+                    if (num != 1)
+                        return false;
+                    else
+                        return true;
+                }
+                else
+                {
+                    num >>= 2;
+                }
+            }
+
+            //either way should not get here
+            return false;
+
+        }
+
+
+    }
+}
